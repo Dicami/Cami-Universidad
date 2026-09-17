6218da638f5edd1b51e02e65d59a8077b25294b0
Dicami <divillagranpa@uide.edu.ec>
2026-09-17 11:36:00 -0500

    Clase

--- Proyectos/funciones/funciones.go
+++ Proyectos/funciones/funciones.go
@@ -2,51 +2,70 @@ package main
 
 import "fmt"
 
-func main() {
-	//saludar()
-	//nombre, edad := datos()
-	//fmt.Println("Hola tu nombre es:", nombre, edad)
-	var a int
-	var b int
-	fmt.Println("Ingrese un numero:")
-	fmt.Scan(&a)
-	fmt.Println("Ingrese el segundo numero:")
-	fmt.Scan(&b)
-	suma := sumar(a, b)
-	fmt.Println("La suma es:", suma)
-}
+/*
+func <nombre>(param1, param2,... param n)<valores de retorno>{
+	-----------------------
+	-----------------------
 
-// Definir una funcion
+	//return en el caso de que nuestra función retorne valores
+	})
+
+*/
+
 func saludar() {
-	//fmt.Println("Hola jaguar you")
-	//var nombre string
-	//fmt.Printf("Ingresa tu nombre")
-	//fmt.Scanln(&nombre)
-	//fmt.Println("Hola", nombre)
-	var a int
-	var b int
-	fmt.Println("Ingrese un numero:")
-	fmt.Scan(&a)
-	fmt.Println("Ingrese el segundo numero:")
-	fmt.Scan(&b)
-	suma := sumar(a, b)
-	fmt.Println("La suma es:", suma)
+	fmt.Println("Hola, esta es mi primera función")
+}
+
+func bienvenida(nombre string) {
+	fmt.Println("Bienvenid@", nombre)
+}
+
+func sumar_restar(num1 int, num2 int) (int, int) {
+	var result_sum int
+	result_sum = num1 + num2
+	if num2 > num1 {
+		var result_rest int
+		result_rest = num2 - num1
+		return result_sum, result_rest
+	} else {
+		fmt.Println("EL resultado es 0")
+		return result_sum, 0
+	}
 
 }
 
-// funcion con retorno
-func datos() (string, int) {
-	var nombre string
-	var edad int
-	fmt.Printf("Ingresa tu nombre")
-	fmt.Scanln(&nombre)
-	fmt.Printf("Ingresa tu edad:")
-	fmt.Scan(&edad)
-	return nombre, edad
+//Variadic function Funciones Variadicas
+
+func mostrarnum(numeros ...int) {
+	fmt.Println("Los numeros ingresados son: ", numeros)
 }
 
-// Datos y argumentos
-func sumar(a int, b int) int {
-	suma := a + b
-	return suma
+func sumatoria(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
+
+func main() {
+	var usr string
+	fmt.Println("Ingresa tu nombre: ")
+	fmt.Scan(&usr)
+	saludar()
+	bienvenida(usr)
+
+	//fmt.Println("El resultado de la suma es: ", sumar(5, 10))
+
+	var num1, num2 int
+	fmt.Println("Ingresa el primer número: ")
+	fmt.Scan(&num1)
+	fmt.Println("Ingresa el segundo número: ")
+	fmt.Scan(&num2)
+	resultadoSuma, resultadoResta := sumar_restar(num1, num2)
+	fmt.Println("Los resultados son: ", resultadoSuma, resultadoResta)
+
+	mostrarnum(5, 10, 15, 20, 25)
+	fmt.Println("La sumatoria es: ", sumatoria(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
 }
